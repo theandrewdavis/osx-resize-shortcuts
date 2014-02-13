8d0604a2fd7d75641fd301e436f973ba07ca0152
Andrew Davis <theandrewdavis@gmail.com>
2014-02-13 09:00:04 -0500

    Cleaned up syntax

--- ResizeShortcuts/main.m
+++ ResizeShortcuts/main.m
@@ -21,29 +21,27 @@
 
 // This callback will be invoked every time there is a keystroke.
 //
-CGEventRef
-myCGEventCallback(CGEventTapProxy proxy, CGEventType type,
-                  CGEventRef event, void *refcon)
-{
+CGEventRef myCGEventCallback(CGEventTapProxy proxy, CGEventType type, CGEventRef event, void *refcon) {
     printf("Key code!\n");
 
     // Paranoid sanity check.
-    if ((type != kCGEventKeyDown) && (type != kCGEventKeyUp))
+    if ((type != kCGEventKeyDown) && (type != kCGEventKeyUp)) {
         return event;
+    }
 
     // The incoming keycode.
-    CGKeyCode keycode = (CGKeyCode)CGEventGetIntegerValueField(
-                                                               event, kCGKeyboardEventKeycode);
+    CGKeyCode keycode = (CGKeyCode)CGEventGetIntegerValueField(event, kCGKeyboardEventKeycode);
 
     // Swap 'a' (keycode=0) and 'z' (keycode=6).
-    if (keycode == (CGKeyCode)0)
+    if (keycode == (CGKeyCode)0) {
         keycode = (CGKeyCode)6;
-    else if (keycode == (CGKeyCode)6)
+    }
+    else if (keycode == (CGKeyCode)6) {
         keycode = (CGKeyCode)0;
+    }
 
     // Set the modified keycode field in the event.
-    CGEventSetIntegerValueField(
-                                event, kCGKeyboardEventKeycode, (int64_t)keycode);
+    CGEventSetIntegerValueField(event, kCGKeyboardEventKeycode, (int64_t)keycode);
 
     // We must return the event for it to be useful.
     return event;
@@ -53,29 +51,26 @@ myCGEventCallback(CGEventTapProxy proxy, CGEventType type,
 int main(int argc, const char **argv) {
     printf("Hello, world\n");
 
-    CFMachPortRef      eventTap;
+/*    CFMachPortRef      eventTap;
     CGEventMask        eventMask;
     CFRunLoopSourceRef runLoopSource;
 
     // Create an event tap. We are interested in key presses.
     eventMask = ((1 << kCGEventKeyDown) | (1 << kCGEventKeyUp));
-    eventTap = CGEventTapCreate(kCGSessionEventTap, kCGHeadInsertEventTap, 0,
-                                eventMask, myCGEventCallback, NULL);
+    eventTap = CGEventTapCreate(kCGSessionEventTap, kCGHeadInsertEventTap, 0, eventMask, myCGEventCallback, NULL);
     if (!eventTap) {
         fprintf(stderr, "failed to create event tap\n");
         exit(1);
     }
 
     // Create a run loop source.
-    runLoopSource = CFMachPortCreateRunLoopSource(
-                                                  kCFAllocatorDefault, eventTap, 0);
+    runLoopSource = CFMachPortCreateRunLoopSource(kCFAllocatorDefault, eventTap, 0);
 
     // Add to the current run loop.
-    CFRunLoopAddSource(CFRunLoopGetCurrent(), runLoopSource,
-                       kCFRunLoopCommonModes);
+    CFRunLoopAddSource(CFRunLoopGetCurrent(), runLoopSource, kCFRunLoopCommonModes);
 
     // Enable the event tap.
-    CGEventTapEnable(eventTap, true);
+    CGEventTapEnable(eventTap, true); */
 
     // Set it all running.
     CFRunLoopRun();
